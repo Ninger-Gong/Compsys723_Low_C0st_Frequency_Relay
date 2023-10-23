45b9f029fb24b642376472794ba6b723cda39c9b
Ning <61758760+Ninger-Gong@users.noreply.github.com>
2023-10-23 21:28:24 +1300

    Add files via upload

---
+++ software/test_freetos/obj/default/freertos_test.d
@@ -0,0 +1,107 @@
+obj/default/freertos_test.o: freertos_test.c ../test_freetos_bsp/system.h \
+  ../test_freetos_bsp/linker.h ../test_freetos_bsp//HAL/inc/io.h \
+  ../test_freetos_bsp//HAL/inc/alt_types.h freertos/FreeRTOS.h \
+  freertos/FreeRTOSConfig.h ../test_freetos_bsp/system.h \
+  freertos/projdefs.h freertos/portable.h \
+  freertos/deprecated_definitions.h freertos/portmacro.h \
+  ../test_freetos_bsp//HAL/inc/sys/alt_irq.h \
+  ../test_freetos_bsp//HAL/inc/nios2.h \
+  ../test_freetos_bsp//HAL/inc/alt_types.h ../test_freetos_bsp/system.h \
+  ../test_freetos_bsp//HAL/inc/priv/alt_legacy_irq.h \
+  ../test_freetos_bsp/system.h ../test_freetos_bsp//HAL/inc/nios2.h \
+  ../test_freetos_bsp//HAL/inc/alt_types.h \
+  ../test_freetos_bsp//HAL/inc/sys/alt_irq.h freertos/mpu_wrappers.h \
+  freertos/task.h freertos/list.h freertos/queue.h freertos/semphr.h \
+  freertos/queue.h FreeRTOS/timers.h FreeRTOS/task.h \
+  ../test_freetos_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  ../test_freetos_bsp//HAL/inc/io.h \
+  ../test_freetos_bsp//drivers/inc/altera_up_avalon_ps2.h \
+  ../test_freetos_bsp//HAL/inc/alt_types.h \
+  ../test_freetos_bsp//HAL/inc/sys/alt_dev.h \
+  ../test_freetos_bsp//HAL/inc/sys/alt_llist.h \
+  ../test_freetos_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../test_freetos_bsp//HAL/inc/sys/alt_llist.h \
+  ../test_freetos_bsp//drivers/inc/altera_up_ps2_keyboard.h \
+  ../test_freetos_bsp//drivers/inc/altera_up_avalon_ps2.h \
+  ../test_freetos_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h \
+  ../test_freetos_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h
+
+../test_freetos_bsp/system.h:
+
+../test_freetos_bsp/linker.h:
+
+../test_freetos_bsp//HAL/inc/io.h:
+
+../test_freetos_bsp//HAL/inc/alt_types.h:
+
+freertos/FreeRTOS.h:
+
+freertos/FreeRTOSConfig.h:
+
+../test_freetos_bsp/system.h:
+
+freertos/projdefs.h:
+
+freertos/portable.h:
+
+freertos/deprecated_definitions.h:
+
+freertos/portmacro.h:
+
+../test_freetos_bsp//HAL/inc/sys/alt_irq.h:
+
+../test_freetos_bsp//HAL/inc/nios2.h:
+
+../test_freetos_bsp//HAL/inc/alt_types.h:
+
+../test_freetos_bsp/system.h:
+
+../test_freetos_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../test_freetos_bsp/system.h:
+
+../test_freetos_bsp//HAL/inc/nios2.h:
+
+../test_freetos_bsp//HAL/inc/alt_types.h:
+
+../test_freetos_bsp//HAL/inc/sys/alt_irq.h:
+
+freertos/mpu_wrappers.h:
+
+freertos/task.h:
+
+freertos/list.h:
+
+freertos/queue.h:
+
+freertos/semphr.h:
+
+freertos/queue.h:
+
+FreeRTOS/timers.h:
+
+FreeRTOS/task.h:
+
+../test_freetos_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../test_freetos_bsp//HAL/inc/io.h:
+
+../test_freetos_bsp//drivers/inc/altera_up_avalon_ps2.h:
+
+../test_freetos_bsp//HAL/inc/alt_types.h:
+
+../test_freetos_bsp//HAL/inc/sys/alt_dev.h:
+
+../test_freetos_bsp//HAL/inc/sys/alt_llist.h:
+
+../test_freetos_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../test_freetos_bsp//HAL/inc/sys/alt_llist.h:
+
+../test_freetos_bsp//drivers/inc/altera_up_ps2_keyboard.h:
+
+../test_freetos_bsp//drivers/inc/altera_up_avalon_ps2.h:
+
+../test_freetos_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h:
+
+../test_freetos_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
